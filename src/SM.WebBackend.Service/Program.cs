df4d421dc3d27f77e5bfd7e6ced9b822cd699305
Andrii Litvinov <andrii.litvinov@gmail.com>
2016-10-30 20:23:45 +0300

    Created project structure.

---
+++ src/SM.WebBackend.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SM.WebBackend.Service
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
