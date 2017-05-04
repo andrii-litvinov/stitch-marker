3f2573a7296d9735bb14caa9e07764ca3b2ec5c6
Andrii Litvinov <andrii.litvinov@gmail.com>
2017-05-04 19:00:38 +0300

    Add MVC to the project.

--- src/SM.Service/Startup.cs
+++ src/SM.Service/Startup.cs
@@ -1,7 +1,5 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -13,28 +11,14 @@ namespace SM.Service
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
-
-            app.UseDefaultFiles().UseStaticFiles();
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path.Value.EndsWith("/")) {
-                    context.Response.Clear();
-
-                    // TODO: Introduce better solution to always return index.html or another default html for route.
-                    await context.Response.WriteAsync(File.ReadAllText("wwwroot/index.html"));
-                } else {
-                    await next();
-                }                
-            });
+            app.UseMvc();
         }
     }
 }
