599212017bb0aa34e2ce1cc704dfc241ac90d0b6
Andrii Litvinov <andrii.litvinov@gmail.com>
2017-05-08 15:09:21 +0300

    Move projects to corresponding folders.

--- test/SM.Xsd.Tests.Unit/XsdPatternParserParseShould.cs
+++ test/SM.Xsd.Tests.Unit/XsdPatternParserParseShould.cs
@@ -1,8 +1,5 @@
-using System;
 using System.IO;
 using FluentAssertions;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using Xunit;
 
 namespace SM.Xsd.Tests.Unit
@@ -17,10 +14,6 @@ namespace SM.Xsd.Tests.Unit
 
             // Act
             var pattern = sut.Read(File.OpenRead("Resources/M198_Seaside beauty.xsd"));
-
-            var json = JsonConvert.SerializeObject(pattern, new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()});
-            File.WriteAllText("pattern.json", json);
-            Console.WriteLine(Path.Combine(Directory.GetCurrentDirectory(), "pattern.json"));
 
             // Assert
             pattern.Stitches.Should().HaveCount(18775);
