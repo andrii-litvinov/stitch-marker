2019ef93280e462c63e41a8f185f1f75362b025d
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2017-06-01 00:19:06 +0300

    added display controls

--- web-app/src/app/canvas/stitch.model.js
+++ web-app/src/app/canvas/stitch.model.js
@@ -3,18 +3,20 @@ class Stitch {
     Object.assign(this, data);
   }
 
-  draw(ctx, size, showSymbol=true) {
+  draw(ctx, size, drawSymbol=true, drawColor=true) {
     let x = toPixel(this.x, size),
       y = toPixel(this.y, size),
       half = size / 2;
 
 
     // draw color
-    ctx.fillStyle = this.color || 'white';
-    ctx.fillRect(x, y, size, size);
+    if (drawColor) {
+      ctx.fillStyle = this.color || 'white';
+      ctx.fillRect(x, y, size, size);
+    }
 
     // draw symbol
-    if (showSymbol && this.symbol) {
+    if (drawSymbol && this.symbol) {
       const symbolWidth = ctx.measureText(this.symbol).width;
       ctx.fillStyle = 'black';
       ctx.font = size * .9 + "px Arial";
