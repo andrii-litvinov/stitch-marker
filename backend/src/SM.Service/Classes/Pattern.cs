57ba129988c8fab86c102bf56ad3e168640404f3
s.v.zapolsky@gmail.com <s.v.zapolsky@gmail.com>
2017-08-25 08:18:53 +0000

    Merged in add-pattern-actor-behaviour (pull request #18)
    Add behaviour to actor
    
    * Add behaviour to actor
    
    * Rename task and clean unuseful code
    
    Approved-by: Andrii Litvinov <andrii.litvinov@gmail.com>

--- backend/src/SM.Service/Classes/Pattern.cs
+++ backend/src/SM.Service/Classes/Pattern.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using System;
 using System.Threading.Tasks;
 using Proto;
 using SM.Core;
@@ -9,25 +9,45 @@ namespace SM.Service.Classes
     public class Pattern : IActor
     {
         private readonly IPatternReader patternReader;
+        private readonly Behavior behavior;
         private PatternState state;
 
         public Pattern(IPatternReader patternReader)
         {
             this.patternReader = patternReader;
+            behavior = new Behavior();
+            behavior.Become(Empty);
         }
 
         public Task ReceiveAsync(IContext context)
+        {
+            return behavior.ReceiveAsync(context);
+        }
+
+        private Task Empty(IContext context)
         {
             switch (context.Message)
             {
                 case CreatePattern pattern:
                     state = patternReader.Read(pattern.Content);
+                    behavior.Become(Created);
+                    break;
+                case PatternQuery _:
+                    break;
+            }
+            return Actor.Done;
+        }
+
+        private Task Created(IContext context)
+        {
+            switch (context.Message)
+            {
+                case CreatePattern pattern:
                     break;
                 case PatternQuery _:
                     context.Respond(state);
                     break;
             }
-            
             return Actor.Done;
         }
     }
