136c9036488be7c3fb99a2151c75be9511c9a695
Stanislav Zapolskyi <s.v.zapolsky@gmail.com>
2017-10-02 20:06:45 +0300

    Clean up code.

--- backend/src/SM.Service/Classes/XsdPatternParser.cs
+++ backend/src/SM.Service/Classes/XsdPatternParser.cs
@@ -9,18 +9,16 @@ namespace SM.Service.Classes
     public class XsdPatternParser : IActor
     {
         private readonly IPatternReader patternReader = new XsdPatternReader();
-        private PatternState pattern;
 
         public Task ReceiveAsync(IContext context)
         {
             if (context.Message is CreatePattern command)
             {
-                pattern = patternReader.Read(command.Content);
+                var pattern = patternReader.Read(command.Content);
                 pattern.Info.Title = command.FileName;
                 pattern.PatternId = command.PatternId;
+                context.Parent.Tell(pattern);
             }
-            context.Parent.Tell(pattern);
-
             return Actor.Done;
         }
     }
