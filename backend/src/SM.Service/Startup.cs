747b1611ce4d238c07896a4aeec59df34648a2d0
kornelandriy <kornelandriy@gmail.com>
2018-04-25 22:23:01 +0300

    Auth implementation. Test version.
    Auth implementation. Test version.

--- backend/src/SM.Service/Startup.cs
+++ backend/src/SM.Service/Startup.cs
@@ -1,39 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 
 namespace SM.Service
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-
-            services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-                })
-                .AddJwtBearer(options =>
-                {
-                    options.Authority = Configuration["Auth0:Authority"];
-                    options.Audience = Configuration["Auth0:Audience"];
-                });
-        }
-
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseAuthentication();
