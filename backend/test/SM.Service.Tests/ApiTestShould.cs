ee4fb60ee5b15e75740950127cde1106cd58958c
kornelandriy <kornelandriy@gmail.com>
2018-05-21 23:38:42 +0300

    Add test of Post method
    Add test of Post method

--- backend/test/SM.Service.Tests/ApiTestShould.cs
+++ backend/test/SM.Service.Tests/ApiTestShould.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.TestHost;
 using System.Net.Http;
 using System.Text;
 using System.Net.Http.Headers;
+using System.IO;
 
 namespace SM.Service.Tests
 {
@@ -17,16 +18,16 @@ namespace SM.Service.Tests
             public string token_type;
         }
 
-        class AuthTokenRequest
+        class AuthTokenRequestData
         {
-            public string client_id = @"AZHrqJ4Qu2tfZ0F4oxljBtaLSv3cJQD1";
-            public string client_secret = @"GsJR7FwkJ-fCQDnhhh3GtwGlM4svMLMzAWDMhCeJZuuzGHzDAhznbIriQ3FF4UNn";
-            public string audience = @"http://localhost:5000/api/";
-            public string grant_type = @"client_credentials";
+            public string client_id = "AZHrqJ4Qu2tfZ0F4oxljBtaLSv3cJQD1";
+            public string client_secret = "GsJR7FwkJ-fCQDnhhh3GtwGlM4svMLMzAWDMhCeJZuuzGHzDAhznbIriQ3FF4UNn";
+            public string audience = "http://localhost:5000/api/";
+            public string grant_type = "client_credentials";
         }
 
         [Fact]
-        public async void ApiTestShouldReturn401Error()
+        public async void ApiTestShouldReturn401()
         {
             using (var client = new TestServer(Program.BuildWebHost()).CreateClient())
             {
@@ -36,21 +37,27 @@ namespace SM.Service.Tests
         }
 
         [Fact]
-        public async void ApiTestShouldNotReturn401Error()
+        public async void ApiTestShouldReturn201()
         {
             using (var client = new TestServer(Program.BuildWebHost()).CreateClient())
             {
                 using (var httpClient = new HttpClient())
                 {
-                    var stringContent = new StringContent(JsonConvert.SerializeObject(new AuthTokenRequest()), Encoding.UTF8, "application/json");
+                    var stringContent = new StringContent(JsonConvert.SerializeObject(new AuthTokenRequestData()), Encoding.UTF8, "application/json");
                     var response = await httpClient.PostAsync("https://stitch-marker.auth0.com/oauth/token", stringContent);
                     var resultContent = response.Content.ReadAsStringAsync().Result;
                     var token = JsonConvert.DeserializeObject<Token>(resultContent).access_token;
 
-                    //client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                    response = await client.GetAsync("/api/patterns/e0de656c-33e7-4ce1-8d5f-118e880d3871");
-                    response.StatusCode.Should().NotBe(401);
+                    var fileStreamContent = new StreamContent(File.OpenRead(@"Resources/M198_Seaside beauty.xsd"));
+                    fileStreamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data") { Name = "file", FileName = "M198_Seaside beauty.xsd" };
+                    fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                    using (var formData = new MultipartFormDataContent())
+                    {
+                        formData.Add(fileStreamContent);
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                        response = await client.PostAsync("/api/patterns/", formData);
+                        response.StatusCode.Should().Be(201);
+                    }
                 }
             }
         }
