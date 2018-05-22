dece56acf554a59a11a387e632e3f2109363ecc8
kornelandriy <kornelandriy@gmail.com>
2018-05-22 21:37:40 +0300

    Auth test methods. Cleaned.
    Auth test methods. Cleaned.

--- backend/test/SM.Service.Tests/ApiTestShould.cs
+++ backend/test/SM.Service.Tests/ApiTestShould.cs
@@ -1,63 +1,63 @@
 ﻿using FluentAssertions;
 using Xunit;
-using Newtonsoft.Json;
 using Microsoft.AspNetCore.TestHost;
 using System.Net.Http;
 using System.Text;
 using System.Net.Http.Headers;
 using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace SM.Service.Tests
 {
-    public class ApiTestShould
+    public class AuthTestShould
     {
-        class Token
+        [Fact]
+        public async Task ApiShouldReturn401()
         {
-            public string access_token;
-            public string expires_in;
-            public string token_type;
-        }
+            // Act
+            var result = await this.MakePostRequestAsync(string.Empty);
 
-        class AuthTokenRequestData
-        {
-            public string client_id = "AZHrqJ4Qu2tfZ0F4oxljBtaLSv3cJQD1";
-            public string client_secret = "GsJR7FwkJ-fCQDnhhh3GtwGlM4svMLMzAWDMhCeJZuuzGHzDAhznbIriQ3FF4UNn";
-            public string audience = "http://localhost:5000/api/";
-            public string grant_type = "client_credentials";
+            // Assert
+            result.StatusCode.Should().Be(401);
         }
 
         [Fact]
-        public async void ApiTestShouldReturn401()
+        public async Task ApiShouldReturn201()
         {
-            using (var client = new TestServer(Program.BuildWebHost()).CreateClient())
+            // Arrange
+            var token = await this.GetAuthTokenAsync();
+
+            // Act
+            var result = await this.MakePostRequestAsync(token);
+
+            // Assert
+            result.StatusCode.Should().Be(201);
+        }
+
+        async Task<string> GetAuthTokenAsync()
+        {
+            using (var httpClient = new HttpClient())
             {
-                var response = await client.GetAsync("http://localhost:5000/api/patterns/e0de656c-33e7-4ce1-8d5f-118e880d3871");
-                response.StatusCode.Should().Be(401);
+                var stringContent = new StringContent("{\"client_id\":\"AZHrqJ4Qu2tfZ0F4oxljBtaLSv3cJQD1\",\"client_secret\":\"GsJR7FwkJ-fCQDnhhh3GtwGlM4svMLMzAWDMhCeJZuuzGHzDAhznbIriQ3FF4UNn\",\"audience\":\"http://localhost:5000/api/\",\"grant_type\":\"client_credentials\"}", Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync("https://stitch-marker.auth0.com/oauth/token", stringContent);
+                var resultContent = await response.Content.ReadAsStringAsync();
+                return JObject.Parse(resultContent).SelectToken("access_token").Value<string>();
             }
         }
 
-        [Fact]
-        public async void ApiTestShouldReturn201()
+        async Task<HttpResponseMessage> MakePostRequestAsync(string token)
         {
             using (var client = new TestServer(Program.BuildWebHost()).CreateClient())
             {
-                using (var httpClient = new HttpClient())
+                var fileStreamContent = new StreamContent(File.OpenRead(@"Resources/M198_Seaside beauty.xsd"));
+                fileStreamContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data") { Name = "file", FileName = "M198_Seaside beauty.xsd" };
+                fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                using (var formData = new MultipartFormDataContent())
                 {
-                    var stringContent = new StringContent(JsonConvert.SerializeObject(new AuthTokenRequestData()), Encoding.UTF8, "application/json");
-                    var response = await httpClient.PostAsync("https://stitch-marker.auth0.com/oauth/token", stringContent);
-                    var resultContent = response.Content.ReadAsStringAsync().Result;
-                    var token = JsonConvert.DeserializeObject<Token>(resultContent).access_token;
-
-                    var fileStreamContent = new StreamContent(File.OpenRead(@"Resources/M198_Seaside beauty.xsd"));
-                    fileStreamContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data") { Name = "file", FileName = "M198_Seaside beauty.xsd" };
-                    fileStreamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
-                    using (var formData = new MultipartFormDataContent())
-                    {
-                        formData.Add(fileStreamContent);
-                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-                        response = await client.PostAsync("/api/patterns/", formData);
-                        response.StatusCode.Should().Be(201);
-                    }
+                    formData.Add(fileStreamContent);
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    return await client.PostAsync("/api/patterns/", formData);
                 }
             }
         }
