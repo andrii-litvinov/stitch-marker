8a5bf969f22e4a5d98104665a0d7ca2b9d716d59
Andrii Litvinov <andrii.litvinov@gmail.com>
2018-08-19 21:23:56 +0300

    Fix zoom

--- web-app/src/app/stitch-canvas/stitch-canvas.js
+++ web-app/src/app/stitch-canvas/stitch-canvas.js
@@ -43,7 +43,7 @@ function shadeBlendConvert(p, from, to) {
   // if (!f || !t) return null; //ErrorCheck
   // if (h) return "rgb(" + r((t[0] - f[0]) * p + f[0]) + "," + r((t[1] - f[1]) * p + f[1]) + "," + r((t[2] - f[2]) * p + f[2]) + (f[3] < 0 && t[3] < 0 ? ")" : "," + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 10000) / 10000 : t[3] < 0 ? f[3] : t[3]) + ")");
   // else return "#" + (0x100000000 + (f[3] > -1 && t[3] > -1 ? r(((t[3] - f[3]) * p + f[3]) * 255) : t[3] > -1 ? r(t[3] * 255) : f[3] > -1 ? r(f[3] * 255) : 255) * 0x1000000 + r((t[0] - f[0]) * p + f[0]) * 0x10000 + r((t[1] - f[1]) * p + f[1]) * 0x100 + r((t[2] - f[2]) * p + f[2])).toString(16).slice(f[3] > -1 || t[3] > -1 ? 1 : 3);
-  return from;
+  return 'white';
 }
 
 function getContrastYIQ(hexcolor) {
--- web-app/src/app/stitch-marker.js
+++ web-app/src/app/stitch-marker.js
@@ -77,7 +77,7 @@ class Marker extends PolymerElement {
     `
   };
 
-  static get zoomLevels() { [.1, .15, .25, .33, .5, .67, .75, .8, .9, 1, 1.1, 1.25, 1.5, 1.75, 2, 3, 4, 5]; }
+  static get zoomLevels() { return [.1, .15, .25, .33, .5, .67, .75, .8, .9, 1, 1.1, 1.25, 1.5, 1.75, 2, 3, 4, 5]; }
 
   static get hotKey() {
     return {
