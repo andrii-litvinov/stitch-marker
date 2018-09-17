b0963099a63544a88009b0ce5ccaef02d338f9fd
kornelandriy <kornelandriy@gmail.com>
2018-09-17 23:08:37 +0300

    Connect to redux-saga

--- web-app/src/app/sagas/index.js
+++ web-app/src/app/sagas/index.js
@@ -1,11 +1,4 @@
-/* eslint-disable no-constant-condition */
-
-import { put, takeEvery } from 'redux-saga/effects'
-
-export function* incrementAsync() {
-  // yield delay(1000);
-  yield put({ type: 'INCREMENT' })
-}
+import { takeEvery } from 'redux-saga/effects'
 
 export default function* rootSaga() {
   yield takeEvery('INCREMENT_ASYNC', incrementAsync)
