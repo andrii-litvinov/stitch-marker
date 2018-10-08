ed698cc6c5bb08f4f6d77b46de68f875a1341d67
kornelandriy <kornelandriy@gmail.com>
2018-10-08 23:18:10 +0300

    Add backstitch mark/unmark

--- web-app/src/app/reducers/index.js
+++ web-app/src/app/reducers/index.js
@@ -29,13 +29,23 @@ const reducer = (state = { pattern: {} }, action) => {
 const updateBackstitch = (state, action) => {
   switch (action.type) {
     case MARK_BACKSTITCHES:
-      
+      state.backstitches.forEach(item => {
+        if (item.x1 == action.x1 &&
+          item.y1 == action.y1 &&
+          item.x2 == action.x2 &&
+          item.y2 == action.y2) item.marked = true;
+      });
       return {
         ...state
       };
 
     case UNMARK_BACKSTITCHES:
-     
+      state.backstitches.forEach(item => {
+        if (item.x1 == action.x1 &&
+          item.y1 == action.y1 &&
+          item.x2 == action.x2 &&
+          item.y2 == action.y2) item.marked = false;
+      });
       return {
         ...state
       };
