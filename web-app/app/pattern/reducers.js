fb967a1a7f448a664826634d6c5655bc01da8db8
Kornylevych Andriy <kornelandriy@gmail.com>
2018-11-20 23:29:06 +0200

    Add stitch_tap action. Add saga watcher for this action.

--- web-app/app/pattern/reducers.js
+++ web-app/app/pattern/reducers.js
@@ -1,10 +1,87 @@
+import { INIT_STORE, MARK_STITCHES, UNMARK_STITCHES, MARK_BACKSTITCHES, UNMARK_BACKSTITCHES, TAP_STITCHES, unmarkStitches, markStitches } from './actions';
 import { combineReducers } from 'redux'
-import backstitches from './backstitch-reducer';
-import stitches from './stitch-reducer';
-import basePatternData from './base-reducer';
+import Backstitch from '../stitch-canvas/backstitch.js';
+import Stitch from '../stitch-canvas/stitch.js';
+import stitches from '../pattern/stitch-reducer'
 
-export default combineReducers({
-  basePatternData,
-  backstitches,
-  stitches
-})
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case INIT_STORE:
+      let backstitches = [];
+      let stitches = [];
+      let backstitchesMap = [];
+
+      const height = action.pattern.height;
+      action.pattern.backstitches.forEach(bs => {
+        const config = action.pattern.configurations[bs.configurationIndex];
+        const strands = config.strands || action.pattern.strands;
+        const backstitch = new Backstitch(config, strands, bs, action.scale, bs.marked);
+        [
+          { x: backstitch.x1, y: backstitch.y1 },
+          { x: backstitch.x2, y: backstitch.y2 }
+        ].forEach(point => {
+          const index = point.x * height + point.y;
+          backstitchesMap[index] = backstitchesMap[index] || [];
+          backstitchesMap[index].push(backstitch);
+          backstitches.push(backstitch);
+        });
+      });
+
+      action.pattern.stitches.forEach(s => {
+        const stitch = new Stitch(action.pattern.configurations[s.configurationIndex], s);
+        stitches[stitch.x * action.pattern.height + stitch.y] = stitch;
+      });
+
+      action.pattern.backstitches = backstitches;
+      action.pattern.backstitchesMap = backstitchesMap;
+      action.pattern.stitches = stitches;
+      return { ...state, pattern: action.pattern };
+
+    case UNMARK_BACKSTITCHES:
+      action.backstitches.forEach(actionBackstitch => {
+        state.pattern.backstitches[actionBackstitch].marked = false;
+      });
+      return { ...state };
+
+    case MARK_BACKSTITCHES:
+      action.backstitches.forEach(actionBackstitch => {
+        state.pattern.backstitches[actionBackstitch].marked = true;
+      });
+      return { ...state };
+
+
+    case TAP_STITCHES:
+      action.stitches.forEach(index => {
+        let stitch = state.pattern.stitches[index];
+        state = updateStitch(state, stitch.marked ? unmarkStitches([index]) : markStitches([index]));
+      });
+      return { ...state };
+
+    default:
+      return state;
+  }
+};
+
+const updateStitch = (state, action) => {
+  switch (action.type) {
+    case UNMARK_STITCHES:
+      action.stitches.forEach(actionStitch => {
+        state.pattern.stitches[actionStitch].marked = false;
+      });
+      return { ...state };
+
+    case MARK_STITCHES:
+      action.stitches.forEach(actionStitch => {
+        state.pattern.stitches[actionStitch].marked = true;
+      });
+      return { ...state };
+  }
+}
+
+// const rootReducer = combineReducers({
+//   reducer,
+//   stitches
+// })
+
+// export default rootReducer;
+export default reducer;
--- web-app/app/stitch-canvas/layers/stitches-layer.js
+++ web-app/app/stitch-canvas/layers/stitches-layer.js
@@ -1,12 +1,16 @@
 import BaseLayer from './base-layer.js';
+import Tile from '../tile.js';
 import { patternStore } from '../../pattern/store.js';
-import { markStitches, unmarkStitches } from '../../pattern/actions.js';
+import { markStitches, unmarkStitches, tapStitches, TAP_STITCHES } from '../../pattern/actions.js';
 
 export default class StitchesLayer extends BaseLayer {
   constructor(scene) {
     super(scene);
-    
-    this.stitches = patternStore.getState().stitches.stitches; 
+
+    this.tiles = [];
+    this.stitches = patternStore.getState().pattern.stitches; 
+
+    this.rearrangeTiles();
 
     const sceneEventListeners = {
       render: e => this.render(e.detail.bounds),
@@ -20,6 +24,42 @@ export default class StitchesLayer extends BaseLayer {
     }
   }
 
+  * watchTapStitch() {
+    yield takeEvery(TAP_STITCHES, tapStitch);
+  }
+
+  tapStitch(e) {
+    this.stitches[e].tap();
+  }
+
+  rearrangeTiles() {
+    this.tiles.forEach(tile => tile.dispose());
+    this.tiles.length = 0;
+
+    let stitchesPerTile = Tile.size / this.scene.stitchSize;
+
+    this.stitches.forEach(stitch => {
+      let column = Math.floor(stitch.x / stitchesPerTile);
+      let row = Math.floor(stitch.y / stitchesPerTile);
+      const spanMultipleTilesX = (stitch.x + 1) * this.scene.stitchSize > (column + 1) * Tile.size;
+      const spanMultipleTilesY = (stitch.y + 1) * this.scene.stitchSize > (row + 1) * Tile.size;
+
+      this.addStitchToTile(row, column, stitch);
+      if (spanMultipleTilesX) this.addStitchToTile(row, column + 1, stitch);
+      if (spanMultipleTilesY) this.addStitchToTile(row + 1, column, stitch);
+      if (spanMultipleTilesY && spanMultipleTilesX) this.addStitchToTile(row + 1, column + 1, stitch);
+    });
+  }
+
+  addStitchToTile(row, column, stitch) {
+    let tile = this.tiles[row * this.scene.pattern.height + column];
+    if (!tile) {
+      tile = new Tile(this, row, column);
+      this.tiles[row * this.scene.pattern.height + column] = tile;
+    }
+    tile.add(stitch);
+  }
+
   zoom(event) {
     this.rearrangeTiles();
     this.render(event.detail.bounds);
@@ -29,11 +69,12 @@ export default class StitchesLayer extends BaseLayer {
     let coordX = Math.floor((event.detail.x - this.scene.x) / this.scene.stitchSize);
     let coordY = Math.floor((event.detail.y - this.scene.y) / this.scene.stitchSize);
     let index = coordX * this.scene.pattern.height + coordY
-    let stitch = this.stitches[index];
+    // let stitch = this.stitches[index];
 
-    dispatch(stitch.marked ? unmarkStitches([index]) : markStitches([index]));
+    // patternStore.dispatch(stitch.marked ? unmarkStitches([index]) : markStitches([index]));
+    patternStore.dispatch(tapStitches([index]));
 
-    if (stitch) stitch.tap();
+    // if (stitch) stitch.tap();
   }
 
   render(bounds) {
